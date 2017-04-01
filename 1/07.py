fe965ccd385c5020bbeac8f3f02a5893eaec0b66
zombiyamo <ars.amnoskmkz@gmail.com>
2017-04-01 17:04:52 +0900

    add 07.py

---
+++ 1/07.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import sys
+
+def createText(x,y,z):
+	print x + "時の" + y + "は" + z
+
+args = sys.argv
+if len(args) == 4:
+	x = args[1]
+	y = args[2]
+	z = args[3]
+ 	createText(x,y,z)
