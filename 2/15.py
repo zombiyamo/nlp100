bf14d732afb302cf2f128a7788291dcc39661e36
zombiyamo <ars.amnoskmkz@gmail.com>
2017-04-02 15:40:18 +0900

    add 15.py

--- 2/15.py
+++ 2/15.py
@@ -4,6 +4,8 @@ import sys
 args = sys.argv
 
 if len(args) == 3:
+    num_lines = sum(1 for i in open(args[1]))
+
     for i, line in enumerate(open(args[1])):
-        if i > len(args[1]) - int(args[2]):
+        if i >= num_lines - int(args[2]):
             print line
